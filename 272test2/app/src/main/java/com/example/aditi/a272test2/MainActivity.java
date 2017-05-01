4b2416cb8877e6175a5b160b0f3404d023d6b6b4
Aditi Bhatia <aditib@uci.edu>
2017-05-01 00:09:51 -0700

    speech -> text display -> gif search/display in app, working

--- 272test2/app/src/main/java/com/example/aditi/a272test2/MainActivity.java
+++ 272test2/app/src/main/java/com/example/aditi/a272test2/MainActivity.java
@@ -1,57 +1,119 @@
 package com.example.aditi.a272test2;
 
+        import java.io.InputStream;
+        import java.net.HttpURLConnection;
+        import java.net.URL;
         import java.util.ArrayList;
-        import android.net.Uri;
+
+        import android.graphics.Movie;
         import java.util.Locale;
         import android.app.Activity;
         import android.content.ActivityNotFoundException;
         import android.content.Intent;
+        import android.net.Uri;
+        import android.os.AsyncTask;
         import android.os.Bundle;
         import android.speech.RecognizerIntent;
         import android.view.View;
+        import android.webkit.WebView;
+        import android.webkit.WebViewClient;
+        import android.widget.Button;
+        import android.widget.EditText;
         import android.widget.ImageButton;
         import android.widget.TextView;
         import android.widget.Toast;
-       // import android.app.Activity;
-        import android.app.SearchManager;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.view.View;
-        import android.widget.EditText;
+        import android.widget.VideoView;
+
+        import javax.json.Json;
+        import javax.json.JsonArray;
+        import javax.json.JsonObject;
+        import javax.json.JsonReader;
+
+        import static android.graphics.Movie.decodeStream;
 
 public class MainActivity extends Activity {
     private final int SPEECH_RECOGNITION_CODE = 1;
     private TextView txtOutput;
     private ImageButton btnMicrophone;
-  //  private EditText editTextInput;
 
+    private WebView back;
+  //  private Button btn;
+    private EditText tmpIn;
+    private String api_path = "http://api.giphy.com/v1/gifs/search?&api_key=dc6zaTOxFJmzC&limit=1";
+    private String query = "&q=";
+
+    /*
+        Download Task
+        Param1: Type of var to send to class DownloadTask
+        Parma2: Name of the method that shows progress
+        Params3: Return var type
+     */
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... params){
+            String gif_url ="";
+            InputStream is;
+            URL url;
+            try {
+                url = new URL(params[0]);
+                is = url.openStream();
+                JsonReader rdr = Json.createReader(is);
+                JsonObject obj = rdr.readObject();
+                JsonArray results = obj.getJsonArray("data");
+                JsonObject result = results.getValuesAs(JsonObject.class).get(0);
+                gif_url = result.getJsonObject("images").getJsonObject("original").getString("mp4");
+
+//                System.out.println(gif_url);
+//                url = new URL(gif_url);
+//                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                connection.connect();
+//                InputStream ls = connection.getInputStream();
+//                gif = decodeStream(ls);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return gif_url;
+        }
+
+        @Override
+        protected void onPostExecute(String gif_url){
+            super.onPostExecute(gif_url);
+            try {
+                back.getSettings().setJavaScriptEnabled(true);
+                back.setWebViewClient(new WebViewClient());
+                back.loadUrl(gif_url);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtOutput = (TextView) findViewById(R.id.txt_output);
         btnMicrophone = (ImageButton) findViewById(R.id.btn_mic);
-        btnMicrophone.setOnClickListener(new View.OnClickListener() {
+       /* btnMicrophone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startSpeechToText();
             }
+        });*/
+
+        back = (WebView) findViewById(R.id.bckgrnd);
+      //  btn = (Button) findViewById(R.id.tempButton);
+        tmpIn = (EditText) findViewById(R.id.tempinput);
+        btnMicrophone.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startSpeechToText();
+
+
+            }
         });
-      //  editTextInput = (EditText) findViewById(R.id.editTextInput);
-
     }
-  /*  public void onSearchClick(View v)
-    {
-        try {
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            String term = editTextInput.getText().toString();
-            intent.putExtra(SearchManager.QUERY, term);
-            startActivity(intent);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-
-    }*/
     /**
      * Start speech to text intent. This opens up Google Speech Recognition API dialog box to listen the speech input.
      * */
@@ -71,14 +133,15 @@ public class MainActivity extends Activity {
                     "Sorry! Speech recognition is not supported on this device.",
                     Toast.LENGTH_SHORT).show();
 
-            String appPackageName = "com.google.android.googlequicksearchbox";
+           /* String appPackageName = "com.google.android.googlequicksearchbox";
            try {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
             } catch (android.content.ActivityNotFoundException anfe) {
                 startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
-            }
+            }*/
         }
     }
+
     /**
      * Callback for speech recognition activity
      * */
@@ -91,20 +154,14 @@ public class MainActivity extends Activity {
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String text = result.get(0);
-                    txtOutput.setText(text);
+                   // txtOutput.setText(text);
+                    tmpIn.setText(text);
 
-
-                  //  Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                  //  intent.putExtra(SearchManager.QUERY,  text); // query contains search string
-                   // startActivity(intent);
-
-                    Uri uri = Uri.parse("https://www.google.com/search?tbm=isch&q="+text);
-                    Intent gSearchIntent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(gSearchIntent);
-
-
-
-
+                    try {
+                        new DownloadTask().execute(api_path + query + tmpIn.getText());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
                 break;
             }
